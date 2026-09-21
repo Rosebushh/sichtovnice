c204d65a23c81655235390bba1c95bb846ca5109
Rosebushh <rosebushh@gmail.com>
2026-09-21 14:43:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// ŠICHTOVNICE - Network-First Service Worker v27
-const CACHE_NAME = 'sichtovnice-v27';
+// ŠICHTOVNICE - Network-First Service Worker v28
+const CACHE_NAME = 'sichtovnice-v28';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
